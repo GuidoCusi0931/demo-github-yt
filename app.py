afaf10c8423efdb67d3ef24b9ba8a18d0a9ad967
Guido Ezequiel Cusi <guido.ezequiel0931@gmail.com>
2026-06-15 02:03:41 +0200

    Conectada la ruta de contacto con base de datos SQLite

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template, request  # <-- ¡Agregamos 'request' aquí!
+from flask import Flask, render_template, request
+import sqlite3  # <-- 1. Importamos la librería de Base de Datos
 
 app = Flask(__name__)
+
+# 2. Función para conectar a la BD y crear la tabla si no existe
+def init_db():
+    conexion = sqlite3.connect('database.db')
+    cursor = conexion.cursor()
+    # Dejamos los campos TEXT limpios y sencillos
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS mensajes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombre TEXT,
+            mensaje TEXT
+        )
+    ''')
+    conexion.commit()
+    conexion.close()
+
+# Ejecutamos la función para que la BD se cree al arrancar la app
+init_db()
+
 
 @app.route('/')
 def inicio():
@@ -10,21 +30,31 @@ def inicio():
 def perfil():
     return render_template('perfil.html', usuario="Guido", rol="Desarrollador Backend")
 
-# <-- Modificamos esta ruta para aceptar GET y POST
+
 @app.route('/contacto', methods=['GET', 'POST'])
 def contacto():
-    mensaje_exito = None  # Por defecto no hay mensaje
+    mensaje_exito = None
     
     if request.method == 'POST':
-        # Capturamos lo que el usuario escribió usando el atributo 'name' del HTML
         nombre_usuario = request.form.get('nombre')
         texto_mensaje = request.form.get('mensaje')
         
-        # Creamos un mensaje para confirmar que recibimos los datos
-        mensaje_exito = f"¡Gracias {nombre_usuario}! Hemos recibido tu mensaje: '{texto_mensaje}'"
+        # 3. GUARDAR EN LA BASE DE DATOS (Usamos INSERT)
+        conexion = sqlite3.connect('database.db')
+        cursor = conexion.cursor()
+        
+        # Los signos '?' son por seguridad, para evitar hackeos (SQL Injection)
+        cursor.execute('''
+            INSERT INTO mensajes (nombre, mensaje) 
+            VALUES (?, ?)
+        ''', (nombre_usuario, texto_mensaje))
+        
+        conexion.commit()  # Guarda los cambios de verdad
+        conexion.close()   # Cierra la conexión siempre
+        
+        mensaje_exito = f"¡Gracias {nombre_usuario}! Tu mensaje fue guardado en la Base de Datos."
 
-    # Le pasamos el mensaje_exito a Jinja2
     return render_template('contacto.html', mensaje_exito=mensaje_exito)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)  # <-- Le decimos que use el puerto 5001
+    app.run(debug=True, port=5001)
